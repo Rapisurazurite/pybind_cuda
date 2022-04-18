efcb773827432dc30b62fdeeeefc3a885b0eb128
Lazurite <lazurite@tom.com>
2022-04-18 23:39:54 +0800

    cpp implementation

--- modules/cpp_extension.cpp
+++ modules/cpp_extension.cpp
@@ -3,11 +3,99 @@
 //
 
 #include "cpp_extension.h"
+#include <vector>
+
+
+at::Tensor d_sigmoid(at::Tensor z) {
+    auto s = at::sigmoid(z);
+    return (1-s)*s;
+}
+
+std::vector<at::Tensor> lltm_forward(
+        torch::Tensor input,
+        torch::Tensor weights,
+        torch::Tensor bias,
+        torch::Tensor old_h,
+        torch::Tensor old_cell){
+    // X = torch.cat([old_h, input], dim=1)
+    auto X = torch::cat({input, old_h}, 1);
+    // gate_weights = F.linear(X, self.weights, self.bias)
+    auto gate_weights = torch::addmm(bias, X, weights.transpose(0, 1));
+    // gates = gate_weights.chunk(3, dim=1)
+    auto gate = gate_weights.chunk(3, 1);
+    // input_gate = torch.sigmoid(gates[0])
+    auto input_gate = torch::sigmoid(gate[0]);
+    // output_gate = torch.sigmoid(gates[1])
+    auto output_gate = torch::sigmoid(gate[1]);
+    // candidate_cell = F.elu(gates[2])
+    auto candidate_cell = torch::elu(gate[2], 1.0);
+    // new_cell = old_cell + candidate_cell * input_gate
+    auto new_cell = old_cell + candidate_cell * input_gate;
+    // new_h = torch.tanh(new_cell) * output_gate
+    auto new_h = torch::tanh(new_cell) * output_gate;
+
+    return {new_h,
+            new_cell,
+            input_gate,
+            output_gate,
+            candidate_cell,
+            X,
+            gate_weights};
+}
+
+// tanh'(z) = 1 - tanh^2(z)
+torch::Tensor d_tanh(torch::Tensor z) {
+    return 1 - z.tanh().pow(2);
+}
+
+// elu'(z) = relu'(z) + { alpha * exp(z) if (alpha * (exp(z) - 1)) < 0, else 0}
+torch::Tensor d_elu(torch::Tensor z, torch::Scalar alpha = 1.0) {
+    auto e = z.exp();
+    auto mask = (alpha * (e - 1)) < 0;
+    return (z > 0).type_as(z) + mask.type_as(z) * (alpha * e);
+}
+
+std::vector<torch::Tensor> lltm_backward(
+        torch::Tensor grad_h,
+        torch::Tensor grad_cell,
+        torch::Tensor new_cell,
+        torch::Tensor input_gate,
+        torch::Tensor output_gate,
+        torch::Tensor candidate_cell,
+        torch::Tensor X,
+        torch::Tensor gate_weights,
+        torch::Tensor weights) {
+    auto d_output_gate = torch::tanh(new_cell) * grad_h;
+    auto d_tanh_new_cell = output_gate * grad_h;
+    auto d_new_cell = d_tanh(new_cell) * d_tanh_new_cell + grad_cell;
+
+    auto d_old_cell = d_new_cell;
+    auto d_candidate_cell = input_gate * d_new_cell;
+    auto d_input_gate = candidate_cell * d_new_cell;
+
+    auto gates = gate_weights.chunk(3, /*dim=*/1);
+    d_input_gate *= d_sigmoid(gates[0]);
+    d_output_gate *= d_sigmoid(gates[1]);
+    d_candidate_cell *= d_elu(gates[2]);
+
+    auto d_gates =
+            torch::cat({d_input_gate, d_output_gate, d_candidate_cell}, /*dim=*/1);
+
+    auto d_weights = d_gates.t().mm(X);
+    auto d_bias = d_gates.sum(/*dim=*/0, /*keepdim=*/true);
+
+    auto d_X = d_gates.mm(weights);
+    const auto state_size = grad_h.size(1);
+    auto d_old_h = d_X.slice(/*dim=*/1, 0, state_size);
+    auto d_input = d_X.slice(/*dim=*/1, state_size);
+
+    return {d_old_h, d_input, d_weights, d_bias, d_old_cell};
+}
 
 void init_module_cpp_extension(pybind11::module &m){
     m.def("is_cpp_extension_init", [](){
         return true;
     });
-
-
+    m.def("forward", &lltm_forward, "LLTM forward");
+    m.def("backward", &lltm_backward, "LLTM backward");
 }
--- test/cpp_extension_test.py
+++ test/cpp_extension_test.py
@@ -1,6 +1,131 @@
 import import_helper
-
+import math
+import time
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import pybind_cuda
 
-assert pybind_cuda.is_cpp_extension_init()
+
+
+class LLTMpy(torch.nn.Module):
+    def __init__(self, input_features, state_size):
+        super(LLTMpy, self).__init__()
+        self.input_features = input_features
+        self.state_size = state_size
+        # 3 * state_size for input gate, output gate and candidate cell gate.
+        # input_features + state_size because we will multiply with [input, h].
+        self.weights = torch.nn.Parameter(
+            torch.empty(3 * state_size, input_features + state_size))
+        self.bias = torch.nn.Parameter(torch.empty(3 * state_size))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1.0 / math.sqrt(self.state_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, +stdv)
+
+    def forward(self, input, state):
+        old_h, old_cell = state
+        X = torch.cat([old_h, input], dim=1)
+
+        # Compute the input, output and candidate cell gates with one MM.
+        gate_weights = F.linear(X, self.weights, self.bias)
+        # Split the combined gate weight matrix into its components.
+        gates = gate_weights.chunk(3, dim=1)
+
+        input_gate = torch.sigmoid(gates[0])
+        output_gate = torch.sigmoid(gates[1])
+        # Here we use an ELU instead of the usual tanh.
+        candidate_cell = F.elu(gates[2])
+
+        # Compute the new cell state.
+        new_cell = old_cell + candidate_cell * input_gate
+        # Compute the new hidden state and output.
+        new_h = torch.tanh(new_cell) * output_gate
+
+        return new_h, new_cell
+
+class LLTMFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input, weights, bias, old_h, old_cell):
+        outputs = pybind_cuda.forward(input, weights, bias, old_h, old_cell)
+        new_h, new_cell = outputs[:2]
+        variables = outputs[1:] + [weights]
+        ctx.save_for_backward(*variables)
+
+        return new_h, new_cell
+
+    @staticmethod
+    def backward(ctx, grad_h, grad_cell):
+        outputs = pybind_cuda.backward(
+            grad_h.contiguous(), grad_cell.contiguous(), *ctx.saved_tensors)
+        d_old_h, d_input, d_weights, d_bias, d_old_cell = outputs
+        return d_input, d_weights, d_bias, d_old_h, d_old_cell
+
+
+class LLTM(torch.nn.Module):
+    def __init__(self, input_features, state_size):
+        super(LLTM, self).__init__()
+        self.input_features = input_features
+        self.state_size = state_size
+        self.weights = torch.nn.Parameter(
+            torch.empty(3 * state_size, input_features + state_size))
+        self.bias = torch.nn.Parameter(torch.empty(3 * state_size))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1.0 / math.sqrt(self.state_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, +stdv)
+
+    def forward(self, input, state):
+        return LLTMFunction.apply(input, self.weights, self.bias, *state)
+
+def python_implementation(num_iter):
+    batch_size = 16
+    input_features = 32
+    state_size = 128
+    X = torch.randn(batch_size, input_features)
+    h = torch.randn(batch_size, state_size)
+    C = torch.randn(batch_size, state_size)
+    rnn = LLTMpy(input_features, state_size)
+    start = time.time()
+    for i in range(num_iter):
+        new_h, new_C = rnn(X, (h, C))
+        (new_h.sum() + new_C.sum()).backward()
+        # h = new_h
+        # C = new_C
+    end = time.time()
+    return end - start
+
+def cpp_implementation(num_iter):
+    batch_size = 16
+    input_features = 32
+    state_size = 128
+    X = torch.randn(batch_size, input_features)
+    h = torch.randn(batch_size, state_size)
+    C = torch.randn(batch_size, state_size)
+    rnn = LLTM(input_features, state_size)
+    start = time.time()
+    for i in range(num_iter):
+        new_h, new_C = rnn(X, (h, C))
+        (new_h.sum() + new_C.sum()).backward()
+        # h = new_h
+        # C = new_C
+    end = time.time()
+    return end - start
+
+
+
+
+if __name__ == '__main__':
+    assert pybind_cuda.is_cpp_extension_init()
+
+    # Python time: 0.6146454811096191
+    py_time = python_implementation(1000)
+    print("Python time: {}".format(py_time))
+
+    # C++ time: 0.4918818473815918
+    cpp_time = cpp_implementation(1000)
+    print("C++ time: {}".format(cpp_time))
